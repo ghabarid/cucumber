db6c9589cb654af12c2b1848916ff2d30e6cc37a
Ridha Ghbarou <rgh@cm-consult.com>
2020-02-08 13:12:23 +0100

    K

--- src/test/java/com/cmc/cucumber/CucumberTest.java
+++ src/test/java/com/cmc/cucumber/CucumberTest.java
@@ -7,9 +7,15 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources",
-        glue = "com.cmc.cucumber"
+    features = "src/test/resources",
+    glue = "com.cmc.cucumber",
+    plugin = {"pretty",
+        "json:target/cucumber-json/cucumber.json",
+        "junit:target/cucumber-reports/Cucumber.xml",
+        "html:target/cucumber-reports"}
+
 //        tags = "@SignUpFeature"
 )
 public class CucumberTest {
+
 }
